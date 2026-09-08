4d6dd6a9c7752e9c0d9ab3aff363045b28c4ed50
Vijay Sampathkumar <56765556+skvijay052@users.noreply.github.com>
2026-09-08 18:57:01 +0530

    Mount homepage motion observer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import { Suspense } from "react";
 import { RouteProgressBar } from "@/components/layout/RouteProgressBar";
 import { MobileRelationshipActionEnhancer } from "@/components/profile/MobileRelationshipActionEnhancer";
+import { HomepageMotion } from "@/components/home/HomepageMotion";
 import "./globals.css";
 import "./mobile-overrides.css";
 import "./home-reference.css";
@@ -30,6 +31,7 @@ export default function RootLayout({
           <RouteProgressBar />
         </Suspense>
         <MobileRelationshipActionEnhancer />
+        <HomepageMotion />
         {children}
       </body>
     </html>
