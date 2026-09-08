603d3c7a9b2d9646488a44b21bff9ab336a3ff0d
Vijay Sampathkumar <56765556+skvijay052@users.noreply.github.com>
2026-09-08 23:12:44 +0530

    Apply original homepage profile photos in code

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,12 @@ import { Suspense } from "react";
 import { RouteProgressBar } from "@/components/layout/RouteProgressBar";
 import { MobileRelationshipActionEnhancer } from "@/components/profile/MobileRelationshipActionEnhancer";
 import { HomepageMotion } from "@/components/home/HomepageMotion";
+import { HomepageProfilePhotos } from "@/components/home/HomepageProfilePhotos";
 import "./globals.css";
 import "./mobile-overrides.css";
 import "./home-reference.css";
 import "./home-horoscope-reference.css";
 import "./discover-showcase.css";
-import "./home-profile-photos.css";
 
 export const metadata: Metadata = {
   title: { default: "Bandhanaa", template: "%s | Bandhanaa" },
@@ -35,6 +35,7 @@ export default function RootLayout({
         </Suspense>
         <MobileRelationshipActionEnhancer />
         <HomepageMotion />
+        <HomepageProfilePhotos />
         {children}
       </body>
     </html>
