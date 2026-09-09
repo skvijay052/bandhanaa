d1509360afb117ac8dc1ecab2bcef48bfb682fcf
Vijay Sampathkumar <56765556+skvijay052@users.noreply.github.com>
2026-09-09 16:23:25 +0530

    Add Discover matrimony page to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,6 +9,12 @@ const publicRoutes = [
     priority: 1,
   },
   {
+    path: "/discover-matrimony",
+    lastModified: "2026-09-09T10:24:00.000Z",
+    changeFrequency: "monthly",
+    priority: 0.9,
+  },
+  {
     path: "/how-it-works",
     lastModified: "2026-09-09T09:20:00.000Z",
     changeFrequency: "monthly",
